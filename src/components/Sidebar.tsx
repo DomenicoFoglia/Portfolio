6c5dfd0334d12c68992f0fa3efc3d147738b0e7a
DomenicoFoglia <foglia.dmnc@gmail.com>
2026-07-20 12:02:13 +0200

    fix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,8 +2,12 @@ import Brand from './Brand'
 import Nav from './Nav'
 import SideFooter from './SideFooter'
 import './Sidebar.css'
+import { useTranslation } from 'react-i18next'
 
 function Sidebar() {
+    const { t } = useTranslation();
+
+
     return (
         <aside className="sidebar" aria-label={t('sidebar.ariaLabel')}>
             <div className="sidebar-top">
