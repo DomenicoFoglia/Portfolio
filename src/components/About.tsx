f30a95e42b08ce409c644b558287f56b6a4213d2
DomenicoFoglia <foglia.dmnc@gmail.com>
2026-07-24 18:07:27 +0200

    Aggiunto sfondo, firma e marginalia

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,24 +1,31 @@
 import Section from './Section'
-import { Trans } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
+import './About.css'
 
 function About() {
+    const { t } = useTranslation();
+
     return (
         <Section
             id="about"
             numeral="I"
             label="About"
         >
-            <p>
+            <p className="p-note">
                 <Trans i18nKey="about.p1" components={{ 1: <strong /> }} />
+                <span className="marginalia">{t('about.note1')}</span>
             </p>
-            <p>
+            <p className="p-note">
                 <Trans i18nKey="about.p2" />
+                <span className="marginalia">{t('about.note2')}</span>
             </p>
-            <p>
+            <p className="p-note">
                 <Trans i18nKey="about.p3" />
+                <span className="marginalia">{t('about.note3')}</span>
             </p>
-            <p>
+            <p className="p-note">
                 <Trans i18nKey="about.p4" />
+                <span className="marginalia">{t('about.note4')}</span>
             </p>
         </Section>
     )
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,7 @@ import Nav from './Nav'
 import SideFooter from './SideFooter'
 import './Sidebar.css'
 import { useTranslation } from 'react-i18next'
+import Signature from './Signature'
 
 function Sidebar() {
     const { t } = useTranslation();
@@ -12,10 +13,11 @@ function Sidebar() {
         <aside className="sidebar" aria-label={t('sidebar.ariaLabel')}>
             <div className="sidebar-top">
                 <Brand />
+                <Signature />
                 <Nav />
             </div>
-        <SideFooter />
-    </aside>
+            <SideFooter />
+        </aside>
     )
 }
 
